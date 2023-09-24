cbd3701166a23d490e4f4ff2c71046ad3db4ab23
asafn2004 <125364822+asafn2004@users.noreply.github.com>
2023-09-25 01:54:21 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  organization: "org-tKDnQy32PnuqFWJJrbJwT1Nn",
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    setIsTyping(true);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+
+    scrollTo(0, 1e10);
+    setMessage("");
+
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are EbereGPT. you help with Email writing.",
+          },
+          ...chats,
+        ],
+      })
+      .then((result) => {
+        console.log(result);
+        msgs.push(result.data.choices[0].message);
+        setChats(msgs);
+        setIsTyping(false);
+        scrollTo(0, 1e10);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <main>
+      <h1>React ChatGPT App</h1>
+
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                <span>{chat.role}</span>
+                <span>:</span>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+      <div className={isTyping ? "" : "hide"}>
+        <p>
+          <i>Typing...</i>
+        </p>
+      </div>
+
+      <form onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message & hit enter"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
+  );
+}
+
+export default App;
